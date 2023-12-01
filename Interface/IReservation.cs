2f0495463cf0f1ac8e12aa73f537133b1123a29f
Malleswari4108 <82399593+Malleswari4108@users.noreply.github.com>
2023-12-01 15:59:53 +0530

    Main Project

---
+++ Interface/IReservation.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication16.Models;
+
+namespace WebApplication16.Interface
+{
+    public interface IReservation
+    {
+        public Task<ActionResult<IEnumerable<Reservation>>> GetReservation();
+
+        public Task<ActionResult<Reservation>> GetReservation(int id);
+
+        public Task<ActionResult<Reservation>> PutReservation(int id, Reservation reservation);
+
+
+        public Task<ActionResult<Reservation>> PostReservation(Reservation reservation);
+        public Task<ActionResult<Reservation>> DeleteReservation(int id);
+        bool ReservationExists(int id);
+    }
+}
---
+++ Repository/CustomerRepo.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.EntityFrameworkCore;
+using WebApplication16.Interface;
+using WebApplication16.Models;
+
+namespace WebApplication16.Repository
+{
+    public class CustomerRepo : ICustomer
+    {
+        private readonly CarDbContext _context;
+        public CustomerRepo(CarDbContext context)
+        {
+
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomer()
+        {
+            return await _context.Customer.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+
+
+            return await _context.Customer.FindAsync(id);
+        }
+
+
+        public async Task<ActionResult<Customer>> PutCustomer(int id, Customer customer)
+        {
+
+
+            _context.Entry(customer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return customer;
+        }
+
+        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        {
+
+            if (_context.Customer.Any(x => x.Username == customer.Username))
+
+            {
+
+                throw new InvalidOperationException($"Username '{customer.Username}' already exists");
+            }
+
+            customer.Password = BCrypt.Net.BCrypt.HashPassword(customer.Password);
+
+            _context.Customer.Add(customer);
+            await _context.SaveChangesAsync();
+            return customer;
+        }
+
+        public async Task<ActionResult<Customer>> DeleteCustomer(int id)
+        {
+            var customer = await _context.Customer.FindAsync(id);
+            _context.Customer.Remove(customer);
+            await _context.SaveChangesAsync();
+
+            return customer;
+        }
+
+        public bool CustomerExists(int id)
+        {
+            return (_context.Customer?.Any(e => e.CustomerId == id)).GetValueOrDefault();
+        }
+
+
+
+
+
+
+
+    }
+}
